69a3cabc0c2d88d4b958f429cebaa01fc0571814
Marcus Appelros <marcus.appelros@gmail.com>
2015-04-15 09:58:48 +0300

    upendo

--- src/Equations.jl
+++ src/Equations.jl
@@ -1,5 +1,5 @@
 module Equations
-export Equation, equation, solve, Expression, expression, evaluate, simplify, simplify!, componify, componify!, Div, Sqrt, addparse, has
+export Equation, equation, solve, Expression, expression, evaluate, simplify, simplify!, componify, componify!, Div, Sqrt, addparse, has, sumnum, sumsym, Components, Component, matches
 
 include("equations.jl")
 
---
+++ test/runtests.jl~
@@ -0,0 +1,5 @@
+using Equations
+using Base.Test
+
+# write your own tests here
+@test 1 == 1
