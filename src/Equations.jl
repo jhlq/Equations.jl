0906c627f8262ce89478cc9dbe9441ae985aeda1
Marcus Appelros <marcus.appelros@gmail.com>
2015-10-23 19:54:31 +0200

    Cosh

--- src/Equations.jl
+++ src/Equations.jl
@@ -1,6 +1,6 @@
 module Equations
 #types
-export Equation, EquationChain, Expression, Component, NonAbelian, Operator, Ex, Term, Factor, ╱,Div, Sqrt, Pow,Exp,Log, Der,DerOp, Vec,Cross,Dot,Norm, Named,Oneable, Physical, AbstractTensor,Tensor,Braket,Delta,⊗,TensorProduct,∧,Wedge,D,Form,Trace,Commutator
+export Equation, EquationChain, Expression, Component, NonAbelian, Operator, Ex, Term, Factor, ╱,Div, Sqrt, Pow,Exp,Log,Cos,Sin,Cosh,Sinh, Der,DerOp, Vec,Cross,Dot,Norm, Named,Oneable, Physical, AbstractTensor,Tensor,Braket,Delta,⊗,TensorProduct,∧,Wedge,D,Form,Trace,Commutator
 #functions
 export equation, solve, expression, ≖, evaluate, simplify, simplify!, componify, componify!, addparse, has, sumnum, sumsym, matches, getarg, findpows, indin, indsin, replace, findsyms, quadratic, complexity, terms, getargs, randeval, expandindices, @equ,@equs
 #relations
--- src/log.jl
+++ src/log.jl
@@ -1,4 +1,4 @@
-import Base: log,exp,cos,sin
+import Base: log,exp,cos,sin,cosh,sinh
 type Log <: Component
 	x
 	y
@@ -53,4 +53,20 @@ function simplify(l::Sin)
 		return sin(x)
 	end
 	return Sin(x)
+end
+for c=((:Cosh,:cosh),(:Sinh,:sinh))
+	ex1=Expr(:type)
+	ex2=Expr(:<:)
+	push!(ex2.args,c[1],Component)
+	ex3=Expr(:block)
+	push!(ex3.args,:x)
+	push!(ex1.args,true,ex2,ex3)
+	eval(ex1)
+	eval(parse("function simplify(l::$(c[1]))
+			x=simplify(l.x)
+			if isa(x,Number)
+				return $(c[2])(x)
+			end
+			return $(c[1])(x)
+		end"))
 end
