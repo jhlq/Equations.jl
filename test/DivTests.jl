1b7b5eeee5ec4db7c670eec433655d429267cb0f
Marcus Appelros <marcus.appelros@gmail.com>
2020-01-10 15:05:50 +0100

    tests

--- test/DivTests.jl
+++ test/DivTests.jl
@@ -1,5 +1,8 @@
 @test simplify(:x/:x,Div)==1
 
+@test simplify(@equ a=c*c/c).rhs==:c
+@test simplify(@equ a=c^2/c).rhs==:c
+
 E=@equ E=sqrt(p^2*c^2+m^2*c^4)
 vars=@equs p=sqrt(2)*1e6/c m=0.5e6/c^2
 r=E&vars[2]
